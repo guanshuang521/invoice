a207bd9abc30d0810f23f26d19b8d5dc222c9f66
shanglingling <1269517390@qq.com>
2019-03-15 15:41:35 +0800

    user修改

--- src/views/system/shop/index.vue
+++ src/views/system/shop/index.vue
@@ -2,6 +2,53 @@
   <div class="dashboard-container">
     <div class="dashboard-text">name:{{ name }}</div>
     <div class="dashboard-text">roles:<span v-for="role in roles" :key="role">{{ role }}</span></div>
+
+    <div style="margin-top:10px;">
+      <el-row>
+        <el-col :span="24"><div class="grid-content bg-purple-dark">
+          <el-row>
+            <el-button type="primary" size="mini">新增</el-button>
+            <el-button type="primary" size="mini">修改</el-button>
+            <el-button type="primary" size="mini">删除</el-button>
+          </el-row>
+        </div></el-col>
+      </el-row>
+    </div>
+    <div class="table" style="padding:5px;">
+      <el-table
+        ref="multipleTable"
+        :data="tableData3"
+        tooltip-effect="dark"
+        style="width: 100%"
+        @selection-change="handleSelectionChange">
+        <el-table-column
+          type="selection"
+          width="55"/>
+        <el-table-column
+          label="日期"
+          width="120">
+          <template slot-scope="scope">{{ scope.row.date }}</template>
+        </el-table-column>
+        <el-table-column
+          prop="tableData3.name"
+          label="姓名"
+          width="120"/>
+        <el-table-column
+          prop="tableData3.address"
+          label="地址"
+          show-overflow-tooltip/>
+      </el-table>
+    </div>
+    <div class="block" style="margin-top:10px;">
+      <el-pagination
+        :total="400"
+        :current-page="currentPage4"
+        :page-sizes="[100, 200, 300, 400]"
+        :page-size="100"
+        layout="total, sizes, prev, pager, next, jumper"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"/>
+    </div>
   </div>
 </template>
 
@@ -9,12 +56,88 @@
 import { mapGetters } from 'vuex'
 
 export default {
-  name: 'Dashboard',
+  name: 'Shop',
+  data() {
+    return {
+      input: '',
+      options: [{
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }, {
+        value: '选项3',
+        label: '蚵仔煎'
+      }, {
+        value: '选项4',
+        label: '龙须面'
+      }, {
+        value: '选项5',
+        label: '北京烤鸭'
+      }],
+      value: '',
+      tableData3: [{
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-08',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-06',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-07',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }],
+      multipleSelection: [],
+      currentPage1: 5,
+      currentPage2: 5,
+      currentPage3: 5,
+      currentPage4: 4
+    }
+  },
   computed: {
     ...mapGetters([
       'name',
       'roles'
     ])
+  },
+  methods: {
+    toggleSelection(rows) {
+      if (rows) {
+        rows.forEach(row => {
+          this.$refs.multipleTable.toggleRowSelection(row)
+        })
+      } else {
+        this.$refs.multipleTable.clearSelection()
+      }
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val
+    },
+    handleSizeChange(val) {
+      // console.log(`每页 ${val} 条`)
+    },
+    handleCurrentChange(val) {
+      // console.log(`当前页: ${val}`)
+    }
   }
 }
 </script>
@@ -28,5 +151,9 @@ export default {
     font-size: 30px;
     line-height: 46px;
   }
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
 }
 </style>
