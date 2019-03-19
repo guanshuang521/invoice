7f5ee7a9f26aa851ffdc7ab1dc2a3f1d15d3c5a6
Mrzhangz <17782007023@163.com>
2019-03-19 14:55:05 +0800

    表格封装

--- src/views/order/components/searchTable.vue
+++ src/views/order/components/searchTable.vue
@@ -3,70 +3,71 @@
  * @Author: zhangzheng
  * @LastEditors: zhangzheng
  * @Date: 2019-03-15 09:50:49
- * @LastEditTime: 2019-03-19 09:33:13
+ * @LastEditTime: 2019-03-19 14:34:09
  -->
 <template>
   <div class="searchTable_wrapper">
     <el-table
-      :data="tableData3"
-      border
-      tooltip-effect="dark"
-      style="width: 100%"
-      @selection-change="handleSelectionChange">
+      ref="refTable"
+
+      :data="dataSource"
+      stripe
+      highlight-current-row
+      @selection-change="handleSelectionChange"
+      @expand-change="expandChange"
+    >
       <el-table-column
         type="selection"
         width="55"/>
-      <el-table-column
-        label="日期"
-        width="120">
-        <!-- <template scope="scope">{{ scope.row.date }}</template> -->
+      <el-table-column label="序号" width="55" align="center">
+        <template slot-scope="scope">
+          <span style="float: right;">{{ scope.$index + 1 }}</span>
+        </template>
       </el-table-column>
+      <!--数据源-->
       <el-table-column
-        prop="name"
-        label="姓名"
-        width="120"/>
-      <el-table-column
-        prop="address"
-        label="地址"
-        show-overflow-tooltip/>
-    </el-table>
-  </div>
-</template>
+        v-for=" column in columns"
+        v-show="column.isShow"
+        :sortable="column.hasSort"
+        :key="column.prop"
+        :prop="column.prop"
+        :label="column.label"
+        :align="column.align"
+        :width="column.width"
+        header-align="center"/>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+            v-for="item in operation.data"
+            :class="item.classname ? item.classname : ''"
+            :key="item.id"
+            :size="item.size"
+            :type="item.type"
+            @click.stop="handleOperation(scope.$index, scope.row, item.id)">{{ item.label }}
+          </el-button>
+        </template>
+    </el-table-column></el-table>
+</div></template>
 
 <script>
 export default {
   name: 'SearchTable',
+  props: {
+    dataSource: { // 表格数据源
+      type: Array,
+      default: () => []
+    },
+    columns: { // 表格的字段展示
+      type: Array,
+      default: () => []
+    },
+    operation: {
+      type: Object,
+      default: () => {}
+    }
+  },
   data() {
     return {
-      tableData3: [{
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-08',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-06',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-07',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }],
       multipleSelection: []
     }
   },
@@ -74,10 +75,31 @@ export default {
   methods: {
     handleSelectionChange(val) {
       this.multipleSelection = val
+      var idsStr = ''
+      for (var i = 0; i < this.multipleSelection.length; i++) {
+        idsStr += this.multipleSelection[i]['username'] + ','
+      }
+      console.log(idsStr)
+    },
+    expandChange() {
+      console.log('wwww')
+    },
+    handleOperation(a, b, id) {
+      const data = this.operation.data
+      for (let i = 0; i < data.length; i++) {
+        if (id === data[i].id) {
+          this.$emit(data[i].Fun, a, b)
+        }
+      }
     }
+
   }
 }
 </script>
 <style rel="stylesheet/scss" lang="scss" scoped>
-
+.searchTable {
+  &_wrapper {
+     padding: 20px;
+  }
+}
 </style>
--- src/views/order/service/index.vue
+++ src/views/order/service/index.vue
@@ -3,12 +3,16 @@
  * @Author: zhangzheng
  * @LastEditors: zhangzheng
  * @Date: 2019-03-13 10:10:12
- * @LastEditTime: 2019-03-19 09:34:24
+ * @LastEditTime: 2019-03-19 14:53:53
  -->
 <template>
   <div class="dashboard-container">
     <search-Form :moudel-type="moudelType" :config="queryConditionsForm" :data.sync="searchConditions"/>
-    <search-Table :table-data3="tableData3" />
+    <search-Table
+      :data-source="dataSource"
+      :columns="columns"
+      :operation="operation"
+      @handleDelete="handleDelete"/>
   </div>
 </template>
 
@@ -89,36 +93,167 @@ export default {
           ]
         }
       ],
-      tableData3: [{
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-08',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-06',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-07',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }],
-      multipleSelection: []
+      dataSource: [
+        {
+          createTime: '2016-05-02',
+          username: '8',
+          status: '上海市普陀区金沙江路 1518 弄',
+          deptName: '家'
+        }, {
+          createTime: '2016-05-04',
+          username: '6',
+          status: '上海市普陀区金沙江路 1517 弄',
+          deptName: '公司'
+        }, {
+          createTime: '2016-05-01',
+          username: '3',
+          status: '上海市普陀区金沙江路 1519 弄',
+          deptName: '家'
+        }, {
+          createTime: '2016-05-03',
+          username: '1',
+          status: '上海市普陀区金沙江路 1516 弄',
+          deptName: '公司'
+        }
+      ], // 数据源
+      columns: [
+        {
+          hasSort: true, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'billCode', // <String>  对应属性名
+          label: '单据编号', // <String>   表头标签
+          align: 'center'
+        },
+        {
+          hasSort: true, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'countOrderNum', // <String>  对应属性名
+          label: '结算单号', // <String>   表头标签
+          align: 'center'
+        },
+        {
+          hasSort: true, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'datatype', // <String>  对应属性名
+          label: '数据类型', // <String>   表头标签
+          align: 'center'
+        },
+        {
+          hasSort: true, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'ordertype', // <String>  对应属性名
+          label: '单据类型', // <String>   表头标签
+          align: 'center'
+        },
+        {
+          hasSort: true, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'OrderNum', // <String>  对应属性名
+          label: '费用单据编号', // <String>   表头标签
+          align: 'center'
+        },
+        {
+          hasSort: true, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'supplierCode', // <String>  对应属性名
+          label: '二级供应商编码', // <String>   表头标签
+          align: 'center'
+        },
+        {
+          hasSort: true, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'money', // <String>  对应属性名
+          label: '金额（不含税）', // <String>   表头标签
+          align: 'center'
+        },
+        {
+          hasSort: true, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'se', // <String>  对应属性名
+          label: '税额', // <String>   表头标签
+          align: 'center',
+          width: 200 // 列宽
+        },
+        {
+          hasSort: true, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'username', // <String>  对应属性名
+          label: '名字', // <String>   表头标签
+          align: 'center'
+        },
+        {
+          hasSort: true, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'deptName', // <String>  对应属性名
+          label: '价税合计', // <String>   表头标签
+          align: 'center'
+        },
+        {
+          hasSort: true, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'status', // <String>  对应属性名
+          label: '销方税号', // <String>   表头标签
+          align: 'center'
+        },
+        {
+          hasSort: true, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'status6', // <String>  对应属性名
+          label: '购方名称', // <String>   表头标签
+          align: 'center'
+        },
+        {
+          hasSort: true, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'status5', // <String>  对应属性名
+          label: '购方税号', // <String>   表头标签
+          align: 'center'
+        },
+        {
+          hasSort: true, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'status4', // <String>  对应属性名
+          label: '购方地址电话', // <String>   表头标签
+          align: 'center'
+        },
+        {
+          hasSort: true, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'status3', // <String>  对应属性名
+          label: '购方开户行及账号', // <String>   表头标签
+          align: 'center'
+        },
+        {
+          hasSort: true, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'status2', // <String>  对应属性名
+          label: '传输日期', // <String>   表头标签
+          align: 'center'
+        },
+        {
+          hasSort: true, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'status1', // <String>  对应属性名
+          label: '备注', // <String>   表头标签
+          align: 'center'
+        }
+      ],
+      operation: { // 操作功能
+        label: '操作', // 操作列的行首文字
+        width: '200', // 操作列的宽度
+        className: '', // 操作列的class名
+        data: [ // 操作列数据
+          {
+            label: '删除', // 按钮文字
+            Fun: 'handleDelete', // 点击按钮后触发的父组件事件
+            size: 'mini', // 按钮大小
+            id: '1', // 按钮循环组件的key值
+            classname: 'show', // 按钮的类名
+            type: 'danger' // 按钮的类型
+          }
+        ]
+      }
+
     }
   },
   computed: {
@@ -130,6 +265,9 @@ export default {
         message: '方法查询',
         type: 'success'
       })
+    },
+    handleDelete(a, b) {
+
     }
   }
 }
