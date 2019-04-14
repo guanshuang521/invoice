2943e87d7ffaa187ac08f782606e9de2e403cd5f
linqunyue <linqy@fapiao.com>
2019-04-14 15:16:45 +0800

    用户超时及无权限提示修改

--- src/utils/request.js
+++ src/utils/request.js
@@ -39,19 +39,14 @@ service.interceptors.response.use(
     if (res.code !== '0000') {
       // 90001:非法的token; 90002:其他客户端登录了;  90003:Token 过期了;
       if (res.code === '90001' || res.code === '90002' || res.code === '90003') {
-        MessageBox.confirm(
-          '你已被登出，可以取消继续留在该页面，或者重新登录',
-          '确定登出',
-          {
-            confirmButtonText: '重新登录',
-            cancelButtonText: '取消',
-            type: 'warning'
+        MessageBox.alert('你已被登出，或者重新登录!', '登录超时', {
+          confirmButtonText: '重新登录', callback: action => {
+            store.dispatch('FedLogOut').then(() => {
+              location.reload() // 为了重新实例化vue-router对象 避免bug
+            })
           }
-        ).then(() => {
-          store.dispatch('FedLogOut').then(() => {
-            location.reload() // 为了重新实例化vue-router对象 避免bug
-          })
         })
+        return false
       }
       return Promise.reject(res.message)
     } else {
