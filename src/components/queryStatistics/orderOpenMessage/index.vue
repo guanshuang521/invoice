cb28918a91992907cade7e02d55c412bde547a44
wujianyun <472323588@qq.com>
2019-05-11 17:10:36 +0800

    订单开票信息查询联调

--- src/components/queryStatistics/orderOpenMessage/index.vue
+++ src/components/queryStatistics/orderOpenMessage/index.vue
@@ -7,7 +7,7 @@
   <div>
     <!-- 新增弹窗 -->
     <el-table
-      :data="list"
+      :data="tableList"
       element-loading-text="Loading"
       border
       fit
@@ -19,76 +19,65 @@
       </el-table-column>
       <el-table-column label="发票代码" align="center">
         <template slot-scope="scope">
-          {{ scope.row.customerName }}
+          {{ scope.row.fpDm }}
         </template>
       </el-table-column>
       <el-table-column label="发票号码" align="center">
         <template slot-scope="scope">
-          <span>{{ scope.row.customerTaxNumber }}</span>
+          <span>{{ scope.row.fpHm }}</span>
         </template>
       </el-table-column>
       <el-table-column label="发票类型" align="center">
         <template slot-scope="scope">
-          {{ scope.row.address }}
+          {{ scope.row.fplx }}
         </template>
       </el-table-column>
       <el-table-column label="金额（不含税）" align="center">
         <template slot-scope="scope">
-          {{ scope.row.email }}
+          {{ scope.row.hjje }}
         </template>
       </el-table-column>
       <el-table-column label="税额" align="center">
         <template slot-scope="scope">
-          {{ scope.row.contacts }}
+          {{ scope.row.hjse }}
         </template>
       </el-table-column>
       <el-table-column label="税价合计" align="center">
         <template slot-scope="scope">
-          {{ scope.row.contactNumber }}
+          {{ scope.row.jshj }}
         </template>
       </el-table-column>
       <el-table-column label="备注" align="center">
         <template slot-scope="scope">
-          {{ scope.row.phone }}
+          {{ scope.row.bz }}
         </template>
       </el-table-column>
     </el-table>
     <div class="page-box">
       <el-pagination
-        :current-page="currentPage"
-        :page-sizes="[10, 20, 30, 50, 100]"
-        :page-size="pageSize"
-        :total="total"
-        layout="prev, pager, next, jumper, total, sizes, slot"
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange">
-        <!-- <span></span> -->
-      </el-pagination>
+        :current-page = "tableCur"
+        :page-sizes = "[1, 10, 20, 30, 50, 100]"
+        :page-size = "tableSize"
+        :total = "tableTotal"
+        style="margin-top: 20px"
+        layout = "total, sizes, prev, pager, next, jumper"
+        @size-change = "handleSizeChange"
+        @current-change = "handleCurrentChange"/>
     </div>
   </div>
 </template>
 <script type="application/ecmascript">
 export default {
   props: {
+    tableList: Array,
+    tableTotal: Number
   },
   data() {
     return {
-      currentPage: 1,
-      pageSize: 25,
-      total: 1000,
+      tableCur: 1,
+      tableSize: 25,
       showDialog: false,
       list: [
-        {
-          djsh: '管理员',
-          xfmc: 1,
-          gfmc: '北京市丰台科技园',
-          xfsh: '12433323454',
-          gfsh: '23543212343',
-          je: '北京银行中关村支行',
-          se: '123444321234567876',
-          jshj: 0,
-          ddzt: 0
-        }
       ]
     }
   },
@@ -101,10 +90,12 @@ export default {
       this.checkedList = val
     },
     handleSizeChange(val) {
-      // console.log(`每页 ${val} 条`)
+      this.$emit('TablePagesizeChange', val)
+      console.log(`每页 ${val} 条`)
     },
     handleCurrentChange(val) {
-      // console.log(`当前页: ${val}`)
+      this.$emit('TableCurrentChange', val)
+      console.log(`当前页: ${val}`)
     },
     handleClose() { // 关闭弹窗
     }
